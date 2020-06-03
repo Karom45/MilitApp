41bee62be950fd9274ddaf5179873487c5a67301
Karom45 <alex.kushnerr@gmail.com>
2020-06-03 22:47:43 +0300

    Adding the activity and filling string file

--- app/src/main/java/com/example/militapp/MainActivity.java
+++ app/src/main/java/com/example/militapp/MainActivity.java
@@ -5,25 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button buttonPlay = (Button)findViewById(R.id.Startbutton);
-        buttonPlay.setOnClickListener(this);
     }
 
-    @Override
-    public void onClick(View view) {
-        if (view.getId() == R.id.Startbutton){
-            Intent i;
-            i = new Intent(this, CalculatingActivity.class);
-            startActivity(i);
+    public void onEnterClick(View View) {
+        Intent intent = new Intent(this, CardsActivity.class);
+        startActivity(intent);
+    }
+
+    public void onRegClick(View View) {
+        Intent intent = new Intent(this, Registration.class);
+        startActivity(intent);
+    }
+    public void onQuitClick(View view) {
+        finish();
         }
     }
-}
+
